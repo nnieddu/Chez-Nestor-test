5e1661bec6294ef6a93fc3cd1339bae5659e431f
nnieddu <nicolas.nieddu@free.fr>
2023-03-03 12:20:10 +0100

    add persistent connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -12,37 +13,78 @@ import LoginForm from "./components/LoginForm";
 
 import "./style/index.css";
 
-const App = () => {
-  const { isLoggedIn } = useContext(PropertyAdsContext);
+async function checkTokenValidity(token: string | null): Promise<boolean> {
+	if (token == null)
+		return false;
 
-  if (!isLoggedIn) {
+  const response = await fetch(
+    `https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=AIzaSyAAltWO-fGFPs41sJe6YyIlV59hIT7MuZU`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        idToken: token,
+      }),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+  const data = await response.json();
+  if (data.hasOwnProperty('error'))
+    return false;
+	return true;
+}
+	
+const App = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(PropertyAdsContext);
+  const [isTokenValid, setIsTokenValid] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("idToken");
+
+    if (token) {
+      checkTokenValidity(token)
+        .then((isValidToken: boolean) => {
+          setIsTokenValid(isValidToken);
+          setIsLoggedIn(isValidToken);
+        })
+        .catch((error: Error) => {
+          console.error("Veuillez vous connecter : " + error);
+        });
+    } else {
+      setIsTokenValid(false);
+    }
+  }, [setIsLoggedIn]);
+
+  if (!isTokenValid && !isLoggedIn) {
     return <LoginForm />;
   }
 
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/ChezNestorImmo"
-            element={
-              <>
-                <Header />
-                <Content />
-              </>
-            }
-          />
-          <Route path="/property/:id" element={<DedicatedPage />} />
-					<Route path="*" element={<Navigate to="/ChezNestorImmo" />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/ChezNestorImmo"
+          element={
+            <>
+              <Header />
+              <Content />
+            </>
+          }
+        />
+        <Route path="/property/:id" element={<DedicatedPage />} />
+        <Route path="*" element={<Navigate to="/ChezNestorImmo" />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
     <PropertyAdsContextProvider>
 		<App />
     </PropertyAdsContextProvider>
-  </React.StrictMode>
 );
