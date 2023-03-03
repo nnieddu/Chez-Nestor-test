2cc1adb5881c7e68dc1f4cbb4afb514b7e925a38
nnieddu <nicolas.nieddu@free.fr>
2023-03-03 10:15:16 +0100

    clean useless line returns, fix modal overflow on mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,14 +15,11 @@ const Modal = ({ isModalOpen, setIsModalOpen, isEdit, propertyAd }: ModalProps) 
       <Dialog as="div" className="z-10 " onClose={() => setIsModalOpen(false)}>
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75" />
         <div className="fixed inset-0 flex items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <Dialog.Panel className="text-left shadow-xl my-8 mx-4 min-w-[85vw] overflow-y-scroll">
+          <Dialog.Panel className="m-auto text-left shadow-xl min-w-[85vw] max-h-full overflow-y-scroll">
             <div className="bg-white rounded-lg sm:flex sm:items-start px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
               <div className="px-5 sm:flex sm:items-start w-full">
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                  <Dialog.Title
-                    as="h2"
-                    className="text-base font-semibold leading-6 text-gray-900"
-                  >
+                  <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
                     Créer votre annonce :
                   </Dialog.Title>
                   <div className="mt-2">
